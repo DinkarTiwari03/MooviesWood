537ed91069e939be9d37aea20eba7f3c271284bf
DinkarTiwari03 <dinkartiwari711@gmail.com>
2025-09-19 00:34:05 +0530

    setup for project and tailwind set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-import React, { use } from 'react'
-import { useState } from 'react'
+import React from "react";  
 
-// this is a functional component/method which is to be called in App function
-const Card = ({ title }) => {
+const App =() => {
   return (
-    <div  className= "card" >
-      <h2> {title}</h2>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+         hello world!
+         </h1>
     </div>
-  )
+  );
 }
 
-
-// this is the main function inside we will call other components/methods
-const App = () => {
-  const [hasLiked, setHasLiked ] = useState(false);
-  return (
-    <div className="card-container">
-
-      <Card title="Star Wars " rating={5} isCool={true} />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
-
-
-    </div>
-
-
-
-  )
-}
-
-export default App
+export default App; 
