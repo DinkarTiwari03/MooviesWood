df47a430c68cc6e84c79745e1e6eeb6e216b0c95
DinkarTiwari03 <dinkartiwari711@gmail.com>
2025-09-22 14:15:49 +0530

    made the background hero-sectiona and search

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,65 @@
-import React from "react";  
+import React, { useEffect, useState } from "react";
+import Search from "./components/search";
 
-const App =() => {
+//API - Application Programming Interface - A set of rules that allows 
+// different software entities to communicate with each other.
+
+const API_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers:{
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+
+}
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async ()=>{
+    try{
+
+    }catch(error){
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Failed to fetch movies. Please try again later.');
+    }
+  }
+
+  useEffect(() => {
+
+
+  },[]);
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-         hello world!
-         </h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient"> Movies </span> You'll Enjoy Without the Hassle </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+        </section>
+
+        
+
+      </div>
+
+
+
+
+    </main>
+
   );
 }
 
